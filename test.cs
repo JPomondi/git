be8d1cd470e31d86b7404e140c341d45b57c2f35
JPomondi <jn.omondi@gmail.com>
2026-07-25 21:37:13 +0300

    another commit

--- test.cs
+++ test.cs
@@ -1,1 +1,16 @@
-Console.WriteLine("Hello, World Again!");
+Console.Write("Enter a number: ");
+string? input = Console.ReadLine();
+
+if (!double.TryParse(input, out double number))
+{
+	Console.WriteLine("That is not a valid number.");
+}
+else if (number < 0)
+{
+	Console.WriteLine("This number does not have a real square root.");
+}
+else
+{
+	double squareRoot = Math.Sqrt(number);
+	Console.WriteLine($"The square root of {number} is {squareRoot}.");
+}
